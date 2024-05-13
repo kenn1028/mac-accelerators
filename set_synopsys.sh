3cd49b0548defa97aa4e2781e2dde93cd1711348
Kenn Danielle Pelayo <kenn1028@gmail.com>
2024-05-13 21:58:51 +0800

    Create set_synopsys.sh

---
+++ set_synopsys.sh
@@ -0,0 +1,17 @@
+#SYNOPSYS Environment Variables
+#License
+setenv LM_LICENSE_FILE 27020@10.158.16.12
+#VCS
+setenv VCS_HOME /cad/tools/synopsys/vcs/T-2022.06-SP2
+setenv VCS_BIN /cad/tools/synopsys/vcs/T-2022.06-SP2/bin
+#SYN
+setenv SYN_BIN /cad/tools/synopsys/syn/U-2022.12/bin
+#LC
+setenv SYNOPSYS_LC_ROOT /cad/tools/synopsys/lc/T-2022.03-SP5
+setenv LC_BIN /cad/tools/synopsys/lc/T-2022.03-SP5/bin
+#PT
+setenv PT_BIN /cad/tools/synopsys/prime/U-2022.12/bin
+#ICC2
+setenv ICC_BIN /cad/tools/synopsys/icc2/T-2022.03-SP05/bin
+#path
+setenv PATH ${PATH}:${VCS_BIN}:${SYN_BIN}:${LC_BIN}:${PT_BIN}:${ICC_BIN}
