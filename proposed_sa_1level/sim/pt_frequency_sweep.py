a7268db2b92378695145b8f241e827bb06e7419f
Kenn Danielle Pelayo <kenn1028@gmail.com>
2024-05-15 13:54:56 +0800

    Initial commit of 1-level implementations

---
+++ proposed_sa_1level/sim/pt_frequency_sweep.py
@@ -0,0 +1,130 @@
+# Import Libraries
+import csv
+import subprocess
+
+# Constants
+area = 538.9128 # in um2
+area_mm2 = area * (1/1000)**2 # in mm2
+
+# Filepaths/Commands
+constraints_path = "../cons/timing.sdc"
+testbench_path = "mac_engine_tb.sv"
+pt_simulation_log_path = "pt_simulation.log"
+pt_report_log_path = "mac_engine_pt_power.rpt"
+vcs_command = "vcs mac_engine_tb.sv ../mapped/mac_engine_mapped.v /cad/tools/libraries/dwc_logic_in_gf22fdx_sc7p5t_116cpp_base_csc20l/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_FDK_RELV02R80/model/verilog/GF22FDX_SC7P5T_116CPP_BASE_CSC20L.v /cad/tools/libraries/dwc_logic_in_gf22fdx_sc7p5t_116cpp_base_csc20l/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_FDK_RELV02R80/model/verilog/prim.v -full64 -debug_pp +neg_tchk -R -l vcs.log"
+
+# Generate Range of Frequencies to Sweep
+interval =    14000000 # 10 MHz
+start_freq = 500000000 # 500 MHz
+end_freq =   570000000 # 560 MHz
+freq_list = list(range(start_freq, (end_freq + interval), interval))
+
+# Create CSV for Energy/Throughput Reports (https://www.freecodecamp.org/news/how-to-create-a-csv-file-in-python/)
+with open('energy_throughput_report.csv', 'w', newline='') as pt_file:
+    writer = csv.writer(pt_file)
+    field = ["Frequency", "Precision", "Power", "Execution Time", "Throughput", "Energy/op", "GOPs/mm2"]
+    writer.writerow(field)
+
+# Sweep through each frequency and precision mode
+prec_list = ['2bx2b', '4bx4b', '8bx8b']
+for freq in freq_list:
+    # Read Constraints File
+    with open(constraints_path, 'r', encoding='utf-8') as cons_file: 
+        constraints_data = cons_file.readlines()
+
+    # Rewrite Constraints File / Clock Frequency
+    constraints_data[1] = "set CLK_FREQUENCY           {}\n".format(freq)
+    with open(constraints_path, 'w', encoding='utf-8') as cons_file: 
+        cons_file.writelines(constraints_data)
+
+    # Perform tests on each precision mode.
+    for prec in prec_list:
+        # Read Testbench File
+        with open(testbench_path, 'r', encoding='utf-8') as tb_file: 
+            tb_data = tb_file.readlines()
+
+        # Change Testbench CLK_PERIOD
+        tb_data[60] = "localparam CLK_PERIOD = " + str(round(int((1/freq)*(1E12)), -1)) + ";\n"
+
+        # Rewrite Testbench based on current precision mode
+        if (prec == '2bx2b'):
+            tb_data[280 + 7] = "    repeat (209) begin\n"
+            tb_data[281 + 7] = "        test_precision(_2bx2b, 8'd13);\n"
+            tb_data[282 + 7] = "    end\n"
+            tb_data[284 + 7] = "    // repeat (209) begin\n"
+            tb_data[285 + 7] = "    //     test_precision(_4bx4b, 8'd13);\n"
+            tb_data[286 + 7] = "    // end\n"
+            tb_data[288 + 7] = "    // repeat (200) begin\n"
+            tb_data[289 + 7] = "    //     test_precision(_8bx8b, 8'd51);\n"
+            tb_data[290 + 7] = "    // end\n"                       
+        elif (prec == '4bx4b'):
+            tb_data[280 + 7] = "    // repeat (209) begin\n"
+            tb_data[281 + 7] = "    //     test_precision(_2bx2b, 8'd13);\n"
+            tb_data[282 + 7] = "    // end\n"
+            tb_data[284 + 7] = "    repeat (209) begin\n"
+            tb_data[285 + 7] = "        test_precision(_4bx4b, 8'd13);\n"
+            tb_data[286 + 7] = "    end\n"
+            tb_data[288 + 7] = "    // repeat (200) begin\n"
+            tb_data[289 + 7] = "    //     test_precision(_8bx8b, 8'd51);\n"
+            tb_data[290 + 7] = "    // end\n" 
+        elif (prec == '8bx8b'):
+            tb_data[280 + 7] = "    // repeat (209) begin\n"
+            tb_data[281 + 7] = "    //     test_precision(_2bx2b, 8'd13);\n"
+            tb_data[282 + 7] = "    // end\n"
+            tb_data[284 + 7] = "    // repeat (209) begin\n"
+            tb_data[285 + 7] = "    //     test_precision(_4bx4b, 8'd13);\n"
+            tb_data[286 + 7] = "    // end\n"
+            tb_data[288 + 7] = "    repeat (200) begin\n"
+            tb_data[289 + 7] = "        test_precision(_8bx8b, 8'd51);\n"
+            tb_data[290 + 7] = "    end\n" 
+
+        with open(testbench_path, 'w', encoding='utf-8') as tb_file: 
+            tb_file.writelines(tb_data)
+
+        # Read power.tcl Script
+        with open("power.tcl", 'r', encoding='utf-8') as tcl_file: 
+            tcl_data = tcl_file.readlines()
+
+        # Change clock period accordingly
+        tcl_data[19] = "create_clock -period " + str(int((1/freq)*(1E12))) +" -name CLK [get_ports clk]\n" # in picoseconds
+
+        with open("power.tcl", 'w', encoding='utf-8') as tcl_file: 
+            tcl_file.writelines(tcl_data)
+
+        # Run Synopsys VCS using the current frequency constraints and precision mode
+        return_code = subprocess.run(vcs_command, shell=True)
+
+        # Run Synopsys PT and the corresponding compile.tcl Script
+        return_code = subprocess.run("pt_shell -file power.tcl", shell=True)
+
+        # Read Synopsys PT Simulation and Analysis Reports
+        with open(pt_simulation_log_path, 'r', encoding='utf-8') as sim_file: 
+            sim_data = sim_file.readlines()
+        with open(pt_report_log_path, 'r', encoding='utf-8') as power_file: 
+            power_data = power_file.readlines()
+
+        # Parse Simulation Log for Execution Time
+        for lines in sim_data:
+            if "Total" in lines.split():
+                exec_time = float(lines.split()[-1]) * (10E-9) # in nanoseconds
+
+        # Parse Power Analysis for Power Report
+        keywords = ["mac_engine", "PSMAC1", "PFU1"]
+        buffer = []
+
+        for lines in power_data:
+            if any(word in lines.split() for word in keywords):
+                buffer.append(lines.split())
+
+        engine_power = float(buffer[1][-2])
+        fu_power = float(buffer[2][-2])
+
+        # Calculate Energy and Throughput
+        energy = ((fu_power*exec_time)/20000)*(1E12) # in pJ
+        throughput = 20000/exec_time
+        throughput_per_area = (throughput/(1E9))/area_mm2 # in GOPs/mm2
+        
+        # Write/Append into CSV file
+        with open('energy_throughput_report.csv', 'a', newline='') as pt_file:
+            writer = csv.writer(pt_file)
+            writer.writerow([freq, prec, fu_power, exec_time, throughput, energy, throughput_per_area])
---
+++ proposed_st_1level/slack_frequency_sweep.py
@@ -0,0 +1,67 @@
+# Import Libraries
+import csv
+import subprocess
+
+# Filepaths
+constraints_path = "cons/timing.sdc"
+area_log_path = "logs/area_report.log"
+timing_log_path = "logs/timing_report.log"
+
+# Generate Range of Frequencies to Sweep
+interval =    10000000 # 1 MHz
+start_freq = 500000000 # 545 MHz
+end_freq =   800000000 # 800 MHz
+freq_list = list(range(start_freq, (end_freq + interval), interval))
+
+# Create CSV for Slack (https://www.freecodecamp.org/news/how-to-create-a-csv-file-in-python/)
+with open('slack_synthesis_report.csv', 'w', newline='') as slack_file:
+    writer = csv.writer(slack_file)
+    field = ["Frequency", "Slack", "Combinational Area", "Buff/Inv Area", "Noncombinational Area", "Total MAC Engine Area", "Total Fusion Unit Area"]
+    writer.writerow(field)
+
+# Sweep through each frequency
+for freq in freq_list:
+    # Read Constraints File
+    with open(constraints_path, 'r', encoding='utf-8') as cons_file: 
+        constraints_data = cons_file.readlines()
+
+    # Rewrite Constraints File / Clock Frequency
+    constraints_data[1] = "set CLK_FREQUENCY           {}\n".format(freq)
+    with open(constraints_path, 'w', encoding='utf-8') as cons_file: 
+        cons_file.writelines(constraints_data)
+
+    # Run Synopsys Design Compiler using the current frequency constraints
+    return_code = subprocess.run("dc_shell -f compile.tcl -output_log_file logs/compile.log", shell=True)
+
+    # Read Timing Reports
+    with open(area_log_path, 'r', encoding='utf-8') as file: 
+        area_data = file.readlines()
+    with open(timing_log_path, 'r', encoding='utf-8') as timing_file: 
+        timing_data = timing_file.readlines()
+
+    # Parse Area Log Reports: Lines 22-24 (Combinational Area, Buff/Inv Area, Noncombinational Area)
+    combi_area = float(area_data[21].split()[-1])
+    buffinv_area = float(area_data[22].split()[-1])
+    noncombi_data = float(area_data[23].split()[-1])
+    print("Combinational Area:", combi_area, "Buff/Inv Area:", buffinv_area, "Noncombinational Area:", noncombi_data)
+
+    # Line 39-40 - (MAC Engine Hierarchy, Fusion Unit Hierarchy)
+    engine_area = float(area_data[38].split()[1])
+    mac_area = float(area_data[39].split()[1])
+    print("MAC Engine Area:", engine_area, "MAC Area:", mac_area)
+
+    # Parse Timing Log Report
+    for lines in timing_data:
+        if "slack" in lines.split():
+            slack = float(lines.split()[2])
+
+    print("Current Frequency Constraints:", freq, "Slack:", slack)
+
+    # Write/Append into CSV file
+    with open('slack_synthesis_report.csv', 'a', newline='') as slack_file:
+        writer = csv.writer(slack_file)
+        writer.writerow([freq, slack, combi_area, buffinv_area, noncombi_data, engine_area, mac_area])
+
+    # If negative slack, end sweep.
+    if slack < 1:
+        break
